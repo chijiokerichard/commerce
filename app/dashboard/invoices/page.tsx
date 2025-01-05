b2a5d7c511eb24480a988544b1aaf6f349369960
chijiokerichard <chijiokrichard61@gmail.com>
2025-01-05 12:34:21 +0100

    my first nextjs app

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return <div>
+        <h1>invoices Page</h1>
+    </div>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter,Lusitana,Bebas_Neue} from "next/font/google"
+
+
+export const inter =  Inter({subsets:["latin"]})
+export const lusitana = Lusitana({subsets:['latin'], weight:['400', '700']})
+export const babas_neue = Bebas_Neue({subsets:['latin'], weight:['400']})
